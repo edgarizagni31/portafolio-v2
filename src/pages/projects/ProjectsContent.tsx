330491d24c2612051401dd93e5b8fa34307e33f4
edgarizagni31 <ochoaedgar3108@gmail.com>
2024-07-05 21:50:14 -0500

    fix: key missing

--- src/pages/projects/ProjectsContent.tsx
+++ src/pages/projects/ProjectsContent.tsx
@@ -1,12 +1,13 @@
-import projects  from "../../api/projects.json";
+import { Children } from "react";
+import projects from "../../api/projects.json";
 import { ProjectItem } from "./ProjectItem";
 
 export const ProjectsContent = () => {
   return (
     <article className="projects__content">
-      {projects.map((project) => (
-        <ProjectItem {...project}  />
-      ))}
+      {Children.toArray(
+        projects.map((project) => <ProjectItem {...project} />)
+      )}
     </article>
   );
 };
