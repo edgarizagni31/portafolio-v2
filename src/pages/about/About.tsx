811abc8b05bf1802cf874976360091f0034177fc
edgarizagni31 <ochoaedgar3108@gmail.com>
2024-07-06 13:26:00 -0500

    fix: text and images

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -8,7 +8,7 @@ export const About = () => {
       <article className="about__interests">
         <h2 className="about__title">Intereses</h2>
         <p className="about__desc">
-          Me encanta el desarrollo web ya que me permite crear aplicaciones 
+          Me encanta el desarrollo web, ya que me permite crear aplicaciones 
           interactivas y  dinámicos que pueden impactar y mejorar la experiencia de usuario de manera significativa. 
           Es emocionante crear soluciones innovadoras que sean útiles y atractivas para los usuarios.<br />
           Soy una persona autodidacta, día a día me dedico tiempo en aprender
